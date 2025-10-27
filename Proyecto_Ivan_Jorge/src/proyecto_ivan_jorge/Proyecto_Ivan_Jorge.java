d8c87244572a745d5babf07a95e1599d19ea7a89
Iván <ivansc06@gmail.com>
2025-10-27 09:40:12 +0100

    Primer commit

---
+++ Proyecto_Ivan_Jorge/src/proyecto_ivan_jorge/Proyecto_Ivan_Jorge.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto_ivan_jorge;
+
+/**
+ *
+ * @author losve
+ */
+public class Proyecto_Ivan_Jorge {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola Mundo!");
+    }
+    
+}
